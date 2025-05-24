a515574166a1064257b9107d70f7621f51d2f196
Vitor <77399148+VitorDAlbuquerque@users.noreply.github.com>
2025-05-24 09:24:09 -0300

    Add files via upload

---
+++ test/example.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe("Exemplo de teste", function(){
+    it('deve somar dois números corretamente', function(){
+        assert.strictEqual(1+1,2)
+    })
+})
