d543019429b17e5e1c8a9857f04281745cdee98f
Vitor <77399148+VitorDAlbuquerque@users.noreply.github.com>
2025-05-24 09:23:56 -0300

    Delete example.test.js

--- example.test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-
-describe("Exemplo de teste", function(){
-    it('deve somar dois números corretamente', function(){
-        assert.strictEqual(1+1,2)
-    })
-})
