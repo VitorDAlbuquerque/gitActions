4adacd3596fad836f8cde855b62697e9528df20a
Vitor <77399148+VitorDAlbuquerque@users.noreply.github.com>
2025-05-24 10:03:43 -0300

    Update example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -2,6 +2,6 @@ const assert = require('assert')
 
 describe("Exemplo de teste", function(){
     it('deve somar dois números corretamente', function(){
-        assert.strictEqual(1+1,2)
+        assert.strictEqual(1+3,2)
     })
 })
